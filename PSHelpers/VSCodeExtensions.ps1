7331275b3eca72a502cd7d278ad2d67c25996a75
Connor Carnes <connorcarnes@pm.me>
2025-04-09 17:02:53 -0500

    update script and helper

--- PSHelpers/VSCodeExtensions.ps1
+++ PSHelpers/VSCodeExtensions.ps1
@@ -1,10 +1,15 @@
 $ChezmoiPath = $(chezmoi source-path)
 $ToInstall = (Get-Content "$ChezmoiPath/.extensions.json" | ConvertFrom-Json)
 $Installed = $(code --list-extensions)
-
+$ToSkip = @(
+    'terrastruct.d2'
+)
 $ToInstall | ForEach-Object {
     if ($_ -in $Installed) {
         Write-Verbose "Extension $_ already installed"
+    }
+    if ($_ -in $ToSkip) {
+        Write-Verbose "Skipping $_"
     }
     else {
         Write-Verbose "Installing extension $_"
@@ -12,8 +17,6 @@ $ToInstall | ForEach-Object {
     }
 }
 
-# $ToSkip = @(
-#     'terrastruct.d2'
 #     'ms-vscode.remote-explorer'
 #     'ms-python.debugpy'
 #     'ms-python.python'
