9fc89332545b0bc1941d61157e6c11aac8da76ea
Connor Carnes <connorcarnes@pm.me>
2025-04-15 04:20:38 -0500

    update pshelpers

--- PSHelpers/VSCodeExtensions.ps1
+++ PSHelpers/VSCodeExtensions.ps1
@@ -1,27 +1,16 @@
-$ChezmoiPath = $(chezmoi source-path)
-$ToInstall = (Get-Content "$ChezmoiPath/.extensions.json" | ConvertFrom-Json)
-$Installed = $(code --list-extensions)
-# $ToSkip = @(
-#     'terrastruct.d2'
-# )
-$ToInstall | ForEach-Object {
-    if ($_ -in $Installed) {
-        Write-Verbose "Extension $_ already installed"
-    }
-    # if ($_ -in $ToSkip) {
-    #     Write-Verbose "Skipping $_"
-    # }
-    else {
-        Write-Verbose "Installing extension $_"
-        $null = code --install-extension $_
-    }
-}
+# $ChezmoiPath = $(chezmoi source-path)
+# $ToInstall = (Get-Content "$ChezmoiPath/.extensions.json" | ConvertFrom-Json)
+# $Installed = $(code --list-extensions)
 
-#     'ms-vscode.remote-explorer'
-#     'ms-python.debugpy'
-#     'ms-python.python'
-#     'ms-vscode-remote.remote-ssh-edit'
-#     'docker.docker'
-#     'ms-vscode-remote.remote-wsl'
-#     'ms-vscode.remote-server'
-# )
+# $ToInstall | ForEach-Object {
+#     if ($_ -in $Installed) {
+#         Write-Verbose "Extension $_ already installed"
+#     }
+
+#     else {
+#         Write-Verbose "Installing extension $_"
+#         $null = code --install-extension $_
+#     }
+# }
+
+
