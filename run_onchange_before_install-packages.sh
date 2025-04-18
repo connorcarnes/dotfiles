6b4f435ebad627c18955d9fd8542ec4f4861e3a0
Connor Carnes <connorcarnes@pm.me>
2025-04-17 19:33:22 -0500

    update bash init script

--- run_onchange_before_install-packages.sh
+++ run_onchange_before_install-packages.sh
@@ -15,3 +15,8 @@ pkgs=(
 
 dnf config-manager --add-repo "https://mise.jdx.dev/rpm/mise.repo"
 dnf install --quiet --assumeyes mise
+
+# mise requires gpg-agent
+# Amazon Linux 2023 comes with gnupg2-minimal which does not include gpg-agent
+# below command swaps out -minimal for the full gnupg2 package that includes gpg-agent
+dnf swap gnupg2-minimal.x86_64 gnupg2.x86_64
